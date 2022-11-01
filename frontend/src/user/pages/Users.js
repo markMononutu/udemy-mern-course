89fbde8021f9d25ccff325a9157fdf76a8effe24
Mark J. Mononutu <s21810053@student.unklab.ac.id>
2022-11-01 20:03:19 +0800

    module 45 Adding a Main Header

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import UserList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Mark M",
+      image: "https://picsum.photos/200",
+      places: 3,
+    },
+  ];
+
+  return <UserList items={USERS} />;
 };
 
 export default Users;
