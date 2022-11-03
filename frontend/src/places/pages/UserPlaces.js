35c8381a5cf36bc15ae8d581599e80b5860b2cec
Mark J. Mononutu <s21810053@student.unklab.ac.id>
2022-11-03 10:52:33 +0800

    rendering user places and using dynamic route segments

---
+++ frontend/src/places/pages/UserPlaces.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Empire_State_Building_04.jpg/640px-Empire_State_Building_04.jpg",
+    address: "20 W 34th St., New York, NY 10001, Amerika Serikat",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Empire_State_Building_04.jpg/640px-Empire_State_Building_04.jpg",
+    address: "20 W 34th St., New York, NY 10001, Amerika Serikat",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    creator: "u2",
+  },
+];
+
+const UserPlaces = () => {
+  return <PlaceList items={DUMMY_PLACES} />;
+};
+
+export default UserPlaces;
