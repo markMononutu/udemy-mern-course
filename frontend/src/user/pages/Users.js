53ee2c707b01f877ac07c0dd5ed47a44b5a1e889
Mark J. Mononutu <s21810053@student.unklab.ac.id>
2022-11-03 08:41:10 +0800

    adding NavLinks

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -7,7 +7,8 @@ const Users = () => {
     {
       id: "u1",
       name: "Mark M",
-      image: "https://picsum.photos/200",
+      image:
+        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
       places: 3,
     },
   ];
